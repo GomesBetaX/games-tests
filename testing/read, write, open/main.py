63a2e57ff04392af5b8af8be63859a71931994a8
gomesbetax <diego.gomesazul@gmail.com>
2025-04-22 22:56:34 -0300

    testin_only

---
+++ testing/read, write, open/main.py
@@ -0,0 +1,21 @@
+# teste de abrir files da maneira clássica
+# file = open("my_file.txt")
+# content = file.read()
+# print(content)
+# file.close()
+
+# to open a file in read mode, you can use the 'with' statement
+with open("my_file.txt") as file:
+    content = file.read()
+    print(content)
+
+# to open a file in write mode, you can use the 'with' statement
+with open("my_file(2).txt", "w") as file:
+    content = file.write("AAAAAA222")
+    print(content)
+
+# to open a file in append mode, you can use the 'with' statement
+with open("my_file.txt", "a") as file:
+    content = file.write("AAAAAA")
+    print(content)
+
