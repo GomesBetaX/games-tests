3fdc4526a15bb75b4880023ca6af721aeb89e495
gomesbetax <diego.gomesazul@gmail.com>
2025-04-25 20:41:35 -0300

    keep_testing

--- us_game/main.py
+++ us_game/main.py
@@ -1,15 +1,18 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+screen.setup(width=800, height=600)
 
 # file path to image
-IMAGE_PATH = "./blank_states_img.gif"
+IMAGE_PATH = "./us_game/blank_states_img.gif"
 
 # add image to screen
 screen.addshape(IMAGE_PATH)
+turtle.shape(IMAGE_PATH)
 
-
+data = pandas.read_csv("./us_game/50_states.csv")
 
 
 # closes screen when window is closed
