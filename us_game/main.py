8e70e351e4266f862f77f5917522efbf98abfcfb
Gomes <diego.gomesazul@gmail.com>
2025-04-25 20:03:36 -0300

    yay

---
+++ us_game/main.py
@@ -0,0 +1,16 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+# file path to image
+IMAGE_PATH = "./blank_states_img.gif"
+
+# add image to screen
+screen.addshape(IMAGE_PATH)
+
+
+
+
+# closes screen when window is closed
+screen.mainloop()
