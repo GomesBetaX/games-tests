a81675c63c9fab71d6cf8688e2c628b2ba2f9035
gomesbetax <diego.gomesazul@gmail.com>
2025-04-28 00:49:27 -0300

    tests in list comprehension

---
+++ testing/list_comprehension/main.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3]
+# key items
+# new_list = [NEW_ITEM_EXPRESSION for ITEM in LIST]
+new_list = [n + 1 for n in numbers]
+
+name = "Angela"
+new_list = [letter for letter in name]
+
+new_range = [n + 1 for n in range(1, 5)]
+print(new_range)
+
