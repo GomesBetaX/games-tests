43ae22e887bf9eb6bd48d378fb1f0806cdfc09ec
gomesbetax <diego.gomesazul@gmail.com>
2025-04-28 18:07:17 -0300

    added list compreheension with if

--- testing/list_comprehension/main.py
+++ testing/list_comprehension/main.py
@@ -9,3 +9,12 @@ new_list = [letter for letter in name]
 new_range = [n + 1 for n in range(1, 5)]
 print(new_range)
 
+# nova lista com conditional
+# new_list = [NEW ITEM EXPRESSION for ITEM in LIST if CONDITION]
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+new_names = [name for name in names if len(name) == 4]
+print(new_names)
+
+# to set a user name in git
+# git config --global user.name "gomesbetax"
+# git config --global user.email "diego.gomesazul@gmail.com"
