0eb98e127dd6347af24fd567b329bc1bb130f4c7
gomesbetax <diego.gomesazul@gmail.com>
2025-05-06 12:58:57 -0300

    fixed pomo logic

--- testing/tkinter/pomodoro/main.py
+++ testing/tkinter/pomodoro/main.py
@@ -12,16 +12,30 @@ LONG_BREAK_MIN = 20
 reps = 0
 
 # ---------------------------- TIMER RESET ----------------------------- # 
-
+def reset_timer():
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------- # 
 def start_timer():
     global reps
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
-
-    count_down(1 * 60)
     reps += 1
+    work_sec = WORK_MIN * 60 #1st, 3rd, 5th, 7th
+    short_break_sec = SHORT_BREAK_MIN * 60 #2nd, 4th, 6th
+    long_break_sec = LONG_BREAK_MIN * 60 #8th
+
+    if reps == 1 or reps == 3 or reps == 5 or reps == 7:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+        
+    elif reps == 2 or reps == 4 or reps == 6:
+        timer_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+
+    elif reps == 8:
+        timer_label.config(text="Rest", fg=RED)
+        count_down(long_break_sec)
+        reset_timer()
+
 
 # ---------------------------- COUNTDOWN MECHANISM --------------------- # 
 def count_down(counter):
@@ -31,6 +45,10 @@ def count_down(counter):
     canvas.itemconfig(timer_text, text=f"{count_min:02}:{count_sec:02}")
     if counter > 0:
         window.after(1000, count_down, counter - 1)
+    else:
+        if reps < 8:
+            start_timer()
+
 
 # ---------------------------- UI SETUP -------------------------------- #
 window = tk.Tk()
